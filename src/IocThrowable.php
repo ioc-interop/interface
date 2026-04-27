796777eb47a3e209c50a6fec526d6d5e4b19abb0
Paul M. Jones <pmjones@pmjones.io>
2026-04-27 11:56:47 -0500

    minor doc reformat

--- src/IocThrowable.php
+++ src/IocThrowable.php
@@ -7,7 +7,9 @@ use Throwable;
 
 /**
  * [_IocThrowable_][] extends [_Throwable_][] to mark an [_Exception_][] as
- * IOC-related. It adds no class members.
+ * IOC-related.
+ *
+ * It adds no class members.
  */
 interface IocThrowable extends Throwable
 {
